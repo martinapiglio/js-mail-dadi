7748a1586d836130507aef1a72b1fc7e90c5336e
martinapiglio <martina.piglio@gmail.com>
2023-02-27 15:47:25 +0100

    dice files scaffolding + pseudo-code

---
+++ dice/js/script.js
@@ -0,0 +1,14 @@
+/*
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+1 - create variables:
+    a - user random number;
+    b - machine random number;
+    c - game outcome (div HTML element);
+
+2 - create an IF condition:
+        a - IF user random number is bigger than machine random number, user wins;
+        b - IF machine random number is bigger than user random number, machine wins;
+        c - IF user random number = machine random number, result is a tie.
+*/
